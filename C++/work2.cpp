1ddaf71366d2577648d2fc1296333c3ed169f7c1
Rukaba <ntwaribrian7@gmail.com>
2025-02-20 20:04:59 +0200

    Update work2.cpp

--- C++/work2.cpp
+++ C++/work2.cpp
@@ -5,7 +5,7 @@ int main() {
     int age = 20;  // Integer variable
     double price = 19.99;  // Double variable
     char grade = 'A';  // Character variable
-    string name = "HESHIMA Herbert";  // String variable
+    string name = "NTWARI Brian";  // String variable
 
     cout << "Name: " << name << endl;
     cout << "Age: " << age << endl;
